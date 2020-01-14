b23e6d9c1d30ba43ec35ff4f082ca33709885b9a
Gemma Vincent <itsme@gemmavincent.co.uk>
2020-01-14 19:07:00 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': ['Montserrat', 'sans-serif'],
+        'heading': ['Baloo', 'sans-serif']
+      },
+      colors: {
+        'brand-green': '#3B8686',
+        'brand-lime': 'rgb(208, 235, 180)',
+        'brand-red': '#de3618',
+        'brand-blue': '#324D5B',
+        'progress-bar': '#28935C'
+      }
+    }
+  },
+  plugins: [],
+  variants: {
+    backgroundColor: ['odd', 'hover', 'focus']
+  }
+}
